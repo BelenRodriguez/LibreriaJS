d119210d95ce5f58c61ab7290554237e50117224
Maria Rodriguez Schachtschneider <mariabelenrodriguez-sch@hotmail.com>
2022-02-21 19:34:26 -0300

    Version Final 2.0

--- js/productos.js
+++ js/productos.js
@@ -3,90 +3,90 @@ let producto = [
       "id": 1,
       "descripcion": "Lápiz",
       "precio": 190,
-      "imagen": "../images/lápiz.jpg"
+      "imagen": "../images/Lápiz.jpg"
     },
     {
       "id": 2,
       "descripcion": "Gomas",
       "precio": 100,
-      "imagen": "../images/gomas.jpg"
+      "imagen": "../images/Gomas.jpg"
     },
     {
       "id": 3,
       "descripcion": "Reglas",
       "precio": 80,
-      "imagen": "../images/reglas.jpg"
+      "imagen": "../images/Reglas.jpg"
     },
     {
       "id": 4,
       "descripcion": "Sacapuntas",
       "precio": 120,
-      "imagen": "../images/sacapuntas.jpg"
+      "imagen": "../images/Sacapuntas.jpg"
     },
     {
       "id": 5,
       "descripcion": "Lapiceras",
       "precio": 60,
-      "imagen": "../images/lapiceras.jpg"
+      "imagen": "../images/Lapiceras.jpg"
     },
     {
       "id": 6,
       "descripcion": "Corrector",
       "precio": 130,
-      "imagen": "../images/corrector.jpg"
+      "imagen": "../images/Corrector.jpg"
     },
     {
         "id": 7,
         "descripcion": "Crayones",
       "precio": 330,
-      "imagen": "../images/crayones.jpg"
+      "imagen": "../images/Crayones.jpg"
     },
     {
       "id": 8,
       "descripcion": "Resaltadores",
       "precio": 240,
-      "imagen": "../images/resaltadores.jpg"
+      "imagen": "../images/Resaltadores.jpg"
     },
     {
       "id": 9,
       "descripcion": "Tijeras",
       "precio": 270,
-      "imagen": "../images/tijeras.jpg"
+      "imagen": "../images/Tijeras.jpg"
     },
     {
       "id": 10,
       "descripcion": "Cartucheras",
       "precio": 2000,
-      "imagen": "../images/cartucheras.jpg"
+      "imagen": "../images/Cartucheras.jpg"
     },
     {
       "id": 11,
       "descripcion": "Cuadernos",
       "precio": 200,
-      "imagen": "../images/cuadernos.jpg"
+      "imagen": "../images/Cuadernos.jpg"
     },
     {
       "id": 12,
       "descripcion": "Carpetas",
       "precio": 1200,
-      "imagen": "../images/carpetas.jpg"
+      "imagen": "../images/Carpetas.jpg"
     },
     {
       "id": 13,
       "descripcion": "Blocks",
       "precio": 340,
-      "imagen": "../images/blocks.jpg"
+      "imagen": "../images/Blocks.jpg"
     },
     {
       "id": 14,
       "descripcion": "Biblioratos",
       "precio": 530,
-      "imagen": "../images/biblioratos.jpg"
+      "imagen": "../images/Biblioratos.jpg"
     },
     {
       "id": 15,
       "descripcion": "Fuelles",
       "precio": 650,
-      "imagen": "../images/fuelles.jpg"
+      "imagen": "../images/Fuelles.jpg"
     }
   ]  
